16364fd5d8b8dfd48601b0fc0fa90e75ebca4805
jnuel <joynuel@gmail.com>
2024-08-16 18:08:38 -0400

    game works! now just gotta add photos and storyline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles.css';
-import { createRef, useState } from 'react';
+import { useState } from 'react';
 import gameData from './GameData';
+import startImage from './images/homescreen_inspo.png';
 
 const personalities_initial_state = {
   "jellycat1": 0,
@@ -9,31 +10,45 @@ const personalities_initial_state = {
   "jellycat4": 0
 };
 
-
 function Game() {
   const [currentState, setCurrentState] = useState(1);
   const [personalities, setPersonalities] = useState(personalities_initial_state);
   const [justStarted, setJustStarted] = useState(true);
   const [storyText, setStoryText] = useState("");
+  const [endGame, setEndGame] = useState("");
+  const [storyImage, setStoryImage] = useState(startImage);
 
   function startGame() {
     console.log("a");
     if (justStarted) {
       setJustStarted(false);
       setStoryText(gameData[currentState].text);
+      setStoryImage(gameData[currentState].image);
+      console.log(storyImage);
       console.log("currentState is ", currentState);
     }
   }
 
-  function renderState() {
-    console.log("b");
-    console.log("current state: ", currentState);
-    if (currentState === 0) {
+  function changeState(newState, selectedPersonalities) {
+    console.log(personalities);
+    console.log("selected personalities: %s", selectedPersonalities);
+    // increase personalities by 1
+    const nextPersonalities = personalities;
+    selectedPersonalities.forEach(personality => {
+      nextPersonalities[personality] = nextPersonalities[personality]+1;
+      console.log("personality: ",personality);
+    })
+
+    setPersonalities(nextPersonalities);
+    setStoryText(gameData[newState].text);  
+    // console.log("new image: ","\""+gameData[newState].image+"\"");
+    console.log(newState)
+
+    setStoryImage(gameData[newState].image);
+    // setStoryImage(require("./smaller_images/2.png"));
+    setCurrentState(newState);
+    if (newState === 0) {
       revealMostSelectedVegetable();
-    }
-    else {
-      console.log("currentState is ", currentState);
-      setStoryText(gameData[currentState].text);
     }
   }
 
@@ -42,41 +57,42 @@ function Game() {
     const buttonList = [];
 
     for (const [choice, info] of Object.entries(gameData[currentState].choices)) {
-      console.log("choices: ", gameData[currentState].choices);
-      console.log("choice: ", choice);
-      console.log("info: ", info);
+      // console.log("choices: ", gameData[currentState].choices);
+      // console.log("choice: ", choice);
+      // console.log("info: ", info);
       let nextState = info[0];
-      buttonList.push(<div className="choice-button" id="choices" onClick={() => changeState(nextState, info[1])}>{choice}</div>);
+      buttonList.push(<div className="choice-button" onClick={() => changeState(nextState, info[1])}>{choice}</div>);
     }
     return buttonList;
   };
 
-
-
-  function changeState(newState, selectedPersonalities) {
-    // console.log(personalities);
-    // console.log("selected personalities %s", selectedPersonalities);
-    // // increase personalities by 1
-    // const nextPersonalities = {...personalities};
-    // for (const key in selectedPersonalities) {
-    //   nextPersonalities[key]++;
-    // }
-    // console.log(selectedPersonalities);
-    // console.log("nextpersonalities post set ", nextPersonalities);
-
-    // setPersonalities(nextPersonalities);
-    // console.log("personalities post set ", personalities);
-    setCurrentState(newState);
-    setStoryText(gameData[newState].text);
-
-    if (newState === 0) {
-      revealMostSelectedVegetable();
-    }
-
-  }
-
   function revealMostSelectedVegetable() {
     console.log("calculating fate");
+    let maxCount = 0;
+    let maxVeggie = '';
+
+    for (const [vegetable, count] of Object.entries(personalities)){
+        if (count > maxCount) {
+            maxCount = count;
+            maxVeggie = vegetable;
+        }
+    }
+    console.log(maxVeggie);
+    setStoryText(`You are a ${maxVeggie}`);
+    const veggieImagePath = `smaller_images/id_cards/${maxVeggie}.png`;
+    setStoryImage(veggieImagePath);
+    setEndGame(true);
+  }
+
+  function createShareButton() {
+    return (<div className="choice-button" id="choices" onClick={() => {
+      const shareMessage = `Check out my Veggie ID! You can create yours at https://jnuel.github.io/jellycat/`;
+            navigator.clipboard.writeText(shareMessage).then(() => {
+                alert('Link copied to clipboard!');
+            }).catch(err => {
+                alert('Failed to copy link. Please try again.');
+            });
+          }}>Share the game with friends</div>);
   }
 
   return (
@@ -84,16 +100,27 @@ function Game() {
       { justStarted ? (
         <div>
           <div className="title">What Jellycat are you?</div>
+          <div className="image-container">
+            <img className="home-screen" src={storyImage} alt="Start"/>
+            </div>
           <a href="#" className="start-button" onClick={startGame}>Start</a>
         </div>
       ) : (
         <div>
-          <div className="game-container" id="game-container"></div>
+          <div className="game-container" id="game-container">
           <div className="story-text" id="story-text">{storyText}</div>
-          <div className="mainChoices">
+          <div className="image-container">
+                <img id="story-image" src={storyImage} alt="Story"/>
+            </div>
               {Object.entries(gameData[currentState].choices)? (
-                <div> {createButtons()}
+                <div className="choices" id="choices"> {createButtons()}
                 </div> ): <div></div>}
+            </div>
+          <div className="end-div">
+            { endGame ? (
+              <div> {createShareButton()}
+              </div>
+            ): <div></div>}
             </div>
         </div>
       )}
--- src/GameData.js
+++ src/GameData.js
@@ -1,27 +1,31 @@
 const gameData = {
     "1": {
-      "text": "It's the beginning of the game.",
+      "text": "It's the beginning of the game. What are you going to choose?",
+      "image": "/smaller_images/1.png",
       "choices": {
-        "ok i'll stay here": [2, ["jellycat1, jellycat2"]],
-        "no": [2, ["jellycat1, jellycat2"]]
+        "ok i'll stay here": [2, ["jellycat1", "jellycat2"]],
+        "no": [2, ["jellycat3", "jellycat2"]]
       }
     },
     "2": {
-      "text": "We're halfway through the game.",
+      "text": "We're halfway through the game. A few more choices to make!",
+      "image": "/smaller_images/2.png",
       "choices": {
-        "left": [3, ["jellycat3"]],
-        "right": [3, ["jellycat3, jellycat4"]]
+        "left": [3, ["jellycat3", "jellycat1"]],
+        "right": [3, ["jellycat3", "jellycat4", "jellycat"]]
       }
     },
     "3": {
-      "text": "Game is about to be over!",
+      "text": "Game is about to be over! Oh no! So sad.",
+      "image": "/smaller_images/3.png",
       "choices": {
-        "idk what": [0, ["jellycat4"]],
-        "okay fine": [0, ["jellycat1, jellycat4"]]
+        "idk what": [0, ["jellycat4", "jellycat2"]],
+        "okay fine": [0, ["jellycat3", "jellycat4"]]
       }
     },
     "0": {
         "text": "",
+        "images": "",
         "choices": {}
     }
   };
